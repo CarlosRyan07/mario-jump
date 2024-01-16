1b9af9b3fa263409f069d4240bd002db98765c3c
CarlosRyan07 <carlosryan.profissional@gmail.com>
2024-01-15 23:45:19 -0300

    atualizando o game

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const clouds = document.querySelector(".clouds");
     const scoreDisplay = document.createElement("div");
     const timerDisplay = document.createElement("div");
+    const keyboardSpace = document.querySelector(".keyboard-space");
+    const keyboardEnter = document.querySelector(".keyboard-enter");
+
     scoreDisplay.classList.add("score");
     timerDisplay.classList.add("timer");
     document.body.appendChild(scoreDisplay);
@@ -20,28 +23,53 @@ document.addEventListener("DOMContentLoaded", () => {
     let pipeSpeed = initialPipeSpeed;
     let cloudsSpeed = initialCloudsSpeed;
 
+    const death = new Audio('audio/death.mp3');
+    const jumping = new Audio('audio/pulo.mp3');
+    const transforming = new Audio('audio/transformando.mp3');
+    const flying = new Audio('audio/som-voando.mp3');    
+    const detransform = new Audio('audio/destransformando.mp3');
+    const theme = new Audio('audio/theme-music.mp3');
+
+    // Ajuste os volumes aqui
+    death.volume = 0.7; // 70% do volume
+    jumping.volume = 0.7;
+    transforming.volume = 1.0; // Volume total (não alterado)
+    flying.volume = 0.5; // 60% do volume
+    detransform.volume = 0.9; 
+    theme.volume = 1.0; // 50% do volume
+
+
     const jump = () => {
         if (!(isJumping || isSsj || isReturningToNormal)) {
+            
+            keyboardSpace.src = "img/keyboard-space-pressed.png";
+            jumping.play();
+
             isJumping = true;
             mario.classList.add("jump");
-    
             setTimeout(() => {
                 mario.style.animation = "none";
                 mario.offsetHeight;
                 mario.classList.remove("jump");
-    
+                keyboardSpace.src = "img/keyboard-space.png";
                 isJumping = false;
             }, 500);
-    
             mario.style.animation = "jump 500ms ease-out";
             mario.style.transformOrigin = "bottom";
         }
     }
-        
+
+   
+   
     let canDetransform = true;
 
     const transformToSsj = () => {
         if (!isJumping) {
+
+            keyboardEnter.src = "img/keyboard-enter-pressed.png";
+
+            transforming.play();
+
             isSsj = true;
             mario.src = "img/mario-transformation.gif"; // Imagem de transformação
             mario.style.bottom = "135px";
@@ -50,6 +78,10 @@ document.addEventListener("DOMContentLoaded", () => {
     
             // Após 3 segundos, mude para a forma SSJ
             setTimeout(() => {
+                keyboardEnter.src = "img/keyboard-enter.png";
+
+                flying.play();
+
                 mario.src = "img/mario-ssj.gif";
                 mario.style.bottom = "180px";
                 mario.style.width = "260px";
@@ -71,6 +103,11 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const transformToNormal = () => {
         if (isSsj && !isJumping && canDetransform) {
+            keyboardEnter.src = "img/keyboard-enter-pressed.png";
+
+            flying.pause();
+            detransform.play();
+            
             isReturningToNormal = true;
             isSsj = false;
             mario.src = "img/back-to-normal.gif";
@@ -79,9 +116,9 @@ document.addEventListener("DOMContentLoaded", () => {
             mario.style.animation = "back-to-normal 3s ease-out";
     
             cloudsSpeed = 0.2;
-            pipeSpeed = 0.2;
     
             setTimeout(() => {
+                keyboardEnter.src = "img/keyboard-enter.png";
                 isReturningToNormal = false;
                 mario.style.bottom = "0";
                 mario.style.width = "150px";
@@ -95,9 +132,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }    
 
 
+    const increaseSpeedThreshold = 30;
+    let speedIncreaseCounter = 0;
+
+
+
     const increaseScore = () => {
         score++;
         scoreDisplay.innerText = `Score: ${score}`;
+
+        // Verifica se atingiu o limite para aumentar a velocidade
+        if (score % increaseSpeedThreshold === 0) {
+            speedIncreaseCounter++;
+            // Ajusta a velocidade do cano e das nuvens
+            pipe.style.animationDuration =  initialPipeSpeed + speedIncreaseCounter * 0.1;
+            clouds.style.animationDuration =  initialPipeSpeed + speedIncreaseCounter * 0.1;
+            //pipeSpeed = initialPipeSpeed + speedIncreaseCounter * 0.1;
+            //cloudsSpeed = initialCloudsSpeed + speedIncreaseCounter * 0.1;
+        }
     }
 
     const restartGame = () => {
@@ -106,6 +158,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const loop = setInterval(() => {
+        theme.play();
+    
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         const cloudsPosition = clouds.offsetLeft;
@@ -134,6 +188,9 @@ document.addEventListener("DOMContentLoaded", () => {
             mario.style.width = "75px";
             mario.style.marginLeft = "51px";
     
+            theme.pause();
+            death.play();
+    
             clearInterval(loop);
     
             document.addEventListener("keydown", restartGame);
@@ -142,6 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (pipePosition <= 0 && !cloudsStopped && !isReturningToNormal) {
             increaseScore();
         }
+        
     
         const currentTime = Date.now();
         const elapsedTime = currentTime - startTime;
@@ -150,6 +208,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
     
         timerDisplay.innerText = `Time: ${minutes}:${seconds}`;
+        
     
         // Ajusta a velocidade do cano com base no tempo decorrido
         pipe.style.animationDuration = `${1.5 / pipeSpeed}s`;
@@ -161,9 +220,8 @@ document.addEventListener("DOMContentLoaded", () => {
         cloudsPosition -= cloudsSpeed;
         clouds.style.left = `${cloudsPosition}px`;
     
-        // Reinicia a posição do cano apenas quando a animação back-to-normal estiver completa
-    
     }, 10);
+    
     
     
     document.addEventListener("keydown", (event) => {
